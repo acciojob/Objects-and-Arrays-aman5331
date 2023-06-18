79d73862221f971a95b09be334b2cab44d78c156
Amandeep Saxena <75635658+aman5331@users.noreply.github.com>
2023-06-18 13:09:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,6 +6,26 @@ const person = {
 };
 
 // Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
+const team = players;
+
+// Creating a copy of the 'players' array
+const team1 = [...players];
+
+// Creating a copy of the 'person' object
+const cap1 = { ...person };
+
+// Modifying the 'team' array
+team.push("Player 4");
+
+console.log(team);   // Output: ["Player 1", "Player 2", "Player 3", "Player 4"]
+console.log(team1);  // Output: ["Player 1", "Player 2", "Player 3"]
+console.log(cap1);   // Output: { name: "John Doe", age: 25, position: "Captain" }
+
+
+
+
+
+
 
 window.players = players;
 window.person = person;
